23227029e9338d4e29919be6d0baefba01a409fd
Ale-Chuli <ale.chulilla.vilar@gmail.com>
2024-08-27 11:32:48 +0200

    user nelmio

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -11,11 +11,20 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Users;
 use App\Repository\UsersRepository;
+use Nelmio\ApiDocBundle\Annotation as Nelmio;
+use OpenApi\Attributes as OA;
 
 #[Route('/user', name: 'user')]
+#[Nelmio\Areas(['internal'])]
+#[OA\Tag('Users')]
 class UserController extends AbstractController
 {
     #[Route('/register', name: 'register', methods: ['POST'])]
+    #[OA\RequestBody(required: true, content: new OA\JsonContent(ref:'#/components/schemas/userRegister'))]
+    #[OA\Response(
+        response: Response::HTTP_CREATED,
+        description: 'User Created')]
+
     public function userRegister(EntityManagerInterface $em, Request $request,
     UserPasswordHasherInterface $passwordHasher): Response
    {
@@ -40,10 +49,16 @@ class UserController extends AbstractController
        $em-> persist($user);
        $em->flush();
 
-       return $this->json("User has been created", Response::HTTP_CREATED);
+       return $this->json("User has been created", 
+       Response::HTTP_CREATED);
    }
 
    #[Route("/login", name:"user_login", methods: ['POST'])]
+   #[OA\RequestBody(required: true, content: new OA\JsonContent(ref:'#/components/schemas/userLogin'))]
+   #[OA\Response(
+    response: Response::HTTP_OK,
+    description: 'Login Successful')]
+
    public function userLogin(UsersRepository $userrep, Request $request,
     UserPasswordHasherInterface $passwordHasher):Response
     {
@@ -55,12 +70,15 @@ class UserController extends AbstractController
         if($user){
             $password = $data['password'];
             if($passwordHasher->isPasswordValid($user,$password)){
-                return $this -> json("Login In ...", Response::HTTP_CREATED);
+                return $this -> json("Login Successful", 
+                Response::HTTP_OK);
             }else{
-                return $this -> json("Invalid Password", Response::HTTP_CREATED);
+                return $this -> json("Invalid Password", 
+                Response::HTTP_UNAUTHORIZED);
             }
             
         }
-        return $this -> json("User doesn't exists", Response::HTTP_CREATED);
+        return $this -> json("User doesn't exists", 
+        Response::HTTP_UNAUTHORIZED);
     }
 }
